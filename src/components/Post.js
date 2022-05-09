d15dbdfe35aa62a244fe31fca50be296fa4acfb2
Matheus Melo <matheuscabal821@gmail.com>
2022-05-09 14:14:33 -0300

    Fixing bugs

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,6 @@
-import React from "react"
-import objetoPost from "./ApiPost"
+import React from "react";
+import objetoPost from "./ApiPost";
+import CommentBar from './CommentBar'
 
 
 function ComponentImg (props){
@@ -18,27 +19,27 @@ function ComponentVideo (props){
     )
 }
 
+// *FUNÇÃO QUE VALIDA SE É IMAGEM OU VIDEO PARA RENDERIZAR CERTO COMPONENTE...
 function IsVideoOrImg(valor, props){
     if (valor === 0){ return <ComponentVideo
         srcPostVideo1={props.srcPostVideo1}
         srcPostVideo1Type={props.srcPostVideo1Type}
         srcPostVideo2={props.srcPostVideo2}
         srcPostVideo2Type={props.srcPostVideo2Type}/>
-    } else { return <ComponentImg contentPostImg={props.contentPostImg}/>}
+    } else { return <ComponentImg contentPostImg={props.contentPostImg}/> }
 }
 
-function Comment(props){ // essa função vai renderizar somente se tiver comentários
+// * Essa função vai renderizar somente se tiver comentários
+function Comment(props){ 
     let arrayComments = [];
-    let namesComment = objetoPost[props.index].nameComment;
-    let comments = objetoPost[props.index].comments;
-    for (let i = 0; i < namesComment.length; i++){
+    let numbersComment = props.nameComment;
+    for (let i = 0; i < numbersComment.length; i++){
         arrayComments.push(
         <div>
-            <span><strong>{namesComment[i]}</strong> {props.comments[i]}</span>
+            <span><strong>{props.nameComment[i]}</strong> {props.comments[i]}</span>
             <ion-icon class='heart-comment' name='heart-outline'></ion-icon>
         </div>)
     }
-    console.log(arrayComments);
     return(
         arrayComments
     )
@@ -47,6 +48,7 @@ function Comment(props){ // essa função vai renderizar somente se tiver coment
 
 
 export default function Post (props){
+    const currentPost = props.objetoPost;
     const [heart, setImg] = React.useState('heart-outline');
 
     function clickLike (){
@@ -57,14 +59,14 @@ export default function Post (props){
         <div>
             <div class="head-post">
                 <div>
-                    <img src={props.imgPerfil} alt="perfi-icon-img"/>
-                    <h5>{props.namePerf}</h5>
+                    <img src={currentPost.imgPerfil} alt="perfi-icon-img"/>
+                    <h5>{currentPost.namePerf}</h5>
                 </div>
                 <img src="./images/points-menu-head-post.svg" alt="points-menu-post"/>
             </div>
 
             <span class='canva-postimg' onDoubleClick={clickLike}>
-            { IsVideoOrImg(props.contentPostImg.length, props)}
+            { IsVideoOrImg(currentPost.contentPostImg.length, currentPost)}
             </span>
     
             <div class="footer-post">
@@ -78,23 +80,17 @@ export default function Post (props){
                 </div>
 
                 <div class="information-post">
-                    <img width= '20px' src={props.imgPerfil} alt={props.altImgPerf}/>
-                    <h5>Curtido por <strong>{props.friendLike}</strong> e <strong>{props.peopleLiked}</strong></h5>
+                    <img width= '20px' src={currentPost.imgPerfil} alt={currentPost.altImgPerf}/>
+                    <h5>Curtido por <strong>{currentPost.friendLike}</strong> e <strong>{currentPost.peopleLiked}</strong></h5>
                 </div>
 
                 <div class="comments">
-                    {Comment(props)}
+                    {Comment(currentPost)}
                 </div>
                 
             </div>
 
-            <div class="comment-bar">
-                <div>
-                    <img width= '28px' src="./images/happy-outline.svg" alt="emoji-icon"/>
-                    <input type="text" placeholder="Add a comment..."/>
-                </div>
-                <button>Publicar</button>
-            </div>
+            <CommentBar />
         </div>
     )
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,22 +4,9 @@ import Post from './Post';
 export default function Posts (){
     return (
         <div class='posts'>
-            { objetoPost.map( (item, index) =>
+            { objetoPost.map( (item) =>
             <Post
-                imgPerfil={item.imgPerfil}
-                namePerf={item.namePerf}
-                contentPostImg={item.contentPostImg}
-                srcPostVideo1={item.srcPostVideo1}
-                srcPostVideo1Type={item.srcPostVideo1Type}
-                srcPostVideo2={item.srcPostVideo2}
-                srcPostVideo2Type={item.srcPostVideo2Type}
-                srcPostVideo3={item.srcPostVideo3}
-                srcPostVideo3Type={item.srcPostVideo3Type}
-                friendLike={item.friendLike}
-                peopleLiked={item.peopleLiked}
-                nameComment={item.nameComment}
-                comments={item.comments}
-                index={index}
+                objetoPost={item}
             />)}
         </div>
     )
